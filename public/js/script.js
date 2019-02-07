40411d16707574b05be58461586236596e608153
nosnayo <n.osnayo@outlook.com>
2019-02-07 13:28:58 -0300

    ojala

--- public/js/script.js
+++ public/js/script.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     $('#alert').hide();
     $(".btn-delete").click(function(e){         //se usa tipo btn para la clase utilizada
         e.preventDefault();                     //captar el click en delete
-        if (!confirm("¿Está seguro de eliminar?")) {
+        /*if (!confirm("¿Está seguro de eliminar?")) {
             return false;
-        }
+        }*/
         
         var row     = $(this).parents('tr');        //para localizar al padre del formulario
         var form    = $(this).parents('form');      //es util porque se usa el atributo que se encuentra ahi
